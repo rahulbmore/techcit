68277706e22fb9f107f7d722a25020a4e85ce03a
c-rahul.m <c-rahul.m@versa-networks.com>
2023-11-28 02:34:54 +0530

    code modified

--- js/main.js
+++ js/main.js
@@ -12,8 +12,7 @@
     spinner();
     
     
-    // Initiate the wowjs
-    new WOW().init();
+    
 
 
     // Sticky Navbar
@@ -25,40 +24,7 @@
         }
     });
     
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-    
+          
     
     // Back to top button
     $(window).scroll(function () {
@@ -68,6 +34,8 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+
+    
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
